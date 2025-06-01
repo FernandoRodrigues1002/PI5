883b0efd2c5ab362b6f44449ca9151532d7194c7
Eric Cordeiro <lele.eric.santos@gmail.com>
2025-06-01 14:08:38 -0300

    NavBar att

--- Front_e_Back/pi5-sem/src/app/components/navbar/Navbar.tsx
+++ Front_e_Back/pi5-sem/src/app/components/navbar/Navbar.tsx
@@ -9,25 +9,24 @@ export default function Navbar() {
     <header className={styles.navbar}>
       <div className={styles.logoArea}>
         <Link href="/">
-          <span className={styles.logoText}>
-            <Image
-              src="/images/logo.png"
-              alt="MedLocator Logo"
-              width={100}
-              height={100}
-              style={{ width: "50px", height: "50px" }}
-            />
-          </span>
+          <Image
+            src="/images/logo.png"
+            alt="MedLocator Logo"
+            width={50}
+            height={50}
+            className={styles.logo}
+          />
         </Link>
       </div>
       <nav className={styles.navLinks}>
         <Link href="/">Início</Link>
         <Link href="/pages/localizar">Localizar</Link>
-        <Link href="/pages/calendario">Calendario</Link>
-        <Link href="/pages/assinatura">assinatura</Link>
-        <Link href="/pages/login">Entrar</Link>
-        <Link href="/pages/sobre">Sobre</Link>
+        <Link href="/pages/calendario">Calendário</Link>
+        <Link href="/pages/assinatura">Assinatura</Link>
       </nav>
+      <div className={styles.authLinks}>
+        <Link href="/pages/login">Entrar</Link>
+      </div>
     </header>
   );
 }
