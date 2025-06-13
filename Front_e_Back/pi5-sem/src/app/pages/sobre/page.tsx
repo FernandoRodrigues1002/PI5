dd1a2ab5e40c76b33522596da5f424ccc8e3490d
Olavo Moretto <olavo.moretto@gmail.com>
2025-06-13 00:52:37 -0300

    PI5 - ajustes finais

--- Front_e_Back/pi5-sem/src/app/pages/sobre/page.tsx
+++ Front_e_Back/pi5-sem/src/app/pages/sobre/page.tsx
@@ -66,7 +66,10 @@ export default function Sobre() {
 
       <div className={styles.history}>
         <h2>Nossa História</h2>
-        <p>história.</p>
+        <p>Em muitas comunidades brasileiras, principalmente nas regiões mais carentes, encontrar um posto de saúde com os medicamentos necessários pode ser uma verdadeira jornada. Faltam informações acessíveis, atualizadas e organizadas. Muitas vezes, o cidadão percorre longas distâncias apenas para descobrir que o remédio que precisa não está disponível.</p>
+        <p>Diante dessa realidade, nasceu o projeto MedLocator — uma plataforma digital desenvolvida com o objetivo de aproximar a população dos serviços de saúde pública, permitindo que qualquer pessoa possa localizar postos de saúde próximos e verificar, em tempo real, quais medicamentos estão disponíveis em cada unidade.</p>
+        <p>O sistema oferece uma experiência simples, rápida e acessível. Basta informar o CEP ou usar a localização atual, e o usuário visualiza os postos de saúde mais próximos, podendo clicar para ver os detalhes dos medicamentos ofertados.</p>
+        <p>Mais do que um projeto de software, o MedLocator representa um compromisso com a inclusão digital, com o fortalecimento do SUS, e com a dignidade das pessoas que dependem do serviço público para cuidar da própria saúde.</p>
       </div>
 
       <h2 className={styles.sectionTitle}>Criadores</h2>
@@ -89,7 +92,7 @@ export default function Sobre() {
       </div>
       <div className={styles.history}>
         <h2>Agradecimentos</h2>
-        <p>Obrigado.</p>
+        <p>Obrigado!</p>
       </div>
       <Footer />
     </>
